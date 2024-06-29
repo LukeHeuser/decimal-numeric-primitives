1204a7b3bd397a4b1d601dd0d05588aceeaed716
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-06-29 19:45:07 -0400

    Printed double and made code easier to read.

--- src/DecimalNumericPrimitives.java
+++ src/DecimalNumericPrimitives.java
@@ -10,20 +10,25 @@ public class DecimalNumericPrimitives {
 //        double last = 10.25_; // does not compile
 
         double myDouble = 2.54;
-        double myDouble2 = 2.54F;
-        double anotherDouble = 2.45D; // d can be used for double, it is optional
+        double myDouble2 = 2.54F; // 'F' represents float
+        double anotherDouble = 2.45D; // 'D' represents double. 'D' can be used for double, it is optional.
 
-        double scientific = 5.000125E03;
-        double scientific2 = 5.000125E3;
-        double myDouble3 = 5000.125;
+        System.out.println("myDouble = " + myDouble);
+        System.out.println("myDouble2 = " + myDouble2);
+        System.out.println("anotherDouble = " + anotherDouble);
 
-        System.out.println("Scientific =" + scientific);
-        System.out.println("Scientific2 =" + scientific2);
-        System.out.println("myDouble3 =" + myDouble3);
+
+        double scientific = 5.000125E03; // 'E' represents exponent. The 0 after the 'E' is optional.
+        double scientific2 = 5.000125E3; // Exemplifying the 0 is optional
+        double myDouble3 = 5000.125; // The
+
+        System.out.println("Scientific = " + scientific);
+        System.out.println("Scientific2 = " + scientific2);
+        System.out.println("myDouble3 = " + myDouble3);
 
         double hexPi = 0x1.91eb851eb851fp1; // p indicates hexadecimal floating point number
 
-        System.out.println("hexPi= " + hexPi);
+        System.out.println("hexPi = " + hexPi);
 
     }
 }

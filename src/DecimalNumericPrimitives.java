b5f504ad1ab212b4f530eda357a719b11fcdadce
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-06-29 20:00:20 -0400

    Another Test Commit

--- src/DecimalNumericPrimitives.java
+++ src/DecimalNumericPrimitives.java
@@ -9,13 +9,14 @@ public class DecimalNumericPrimitives {
 //        double first = _10.25; // does not compile
 //        double last = 10.25_; // does not compile
 
-        System.out.println();
-
         double myDouble = 2.54;
         double myDouble2 = 2.54F; // 'F' represents float
         double anotherDouble = 2.45D; // 'D' represents double. 'D' can be used for double, it is optional.
 
-        System.out.println("myDouble = ");
+        System.out.println("myDouble = " + myDouble);
+        System.out.println("myDouble2 = " + myDouble2);
+        System.out.println("anotherDouble = " + anotherDouble);
+
 
         double scientific = 5.000125E03; // 'E' represents exponent. The 0 after the 'E' is optional.
         double scientific2 = 5.000125E3; // Exemplifying the 0 is optional
